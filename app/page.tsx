41849b87728d8ae94eaa54ee5fa11e1970fd6f24
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-11-10 09:02:44 +0330

    feat: home page designed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import ProgrammerIcon from "@/assets/svg/ProgrammerIcon";
 const Home = () => {
-  return <h1>Next TS Challenges</h1>;
+  return (
+    <div className="pt-16 flex flex-col justify-center items-center text-center gap-8">
+      <h1 className="font-bold text-3xl">Welcome to NextTS Challenges</h1>
+      <div className="w-60 h-60 [&_*]:h-full [&_*]:w-full">
+        <ProgrammerIcon />
+      </div>
+      <h3 className="font-bold text-2xl">
+        Navigate between routes to see the different challenges.
+      </h3>
+    </div>
+  );
 };
 
 export default Home;
