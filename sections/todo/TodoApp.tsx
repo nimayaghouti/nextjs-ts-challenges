b1f518db80d90c7c87d59dfc705898e739355c20
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-11-11 15:30:45 +0330

    feat: add pagination to todolist using usepagination hook

--- sections/todo/TodoApp.tsx
+++ sections/todo/TodoApp.tsx
@@ -2,8 +2,10 @@
 import { useState, useMemo } from "react";
 import TodoOptions from "./TodoOptions";
 import TodoList from "./TodoList";
+import Pagination from "@/components/Pagination";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import usePagination from "@/hooks/usePagination";
 
 const TodoApp = () => {
   const [status, setStatus] = useState<string>("all");
@@ -17,6 +19,9 @@ const TodoApp = () => {
       : tasks;
   }, [status, tasks]);
 
+  const { totalPages, currentPage, currentPageData, handlePageChange } =
+    usePagination(filteredTasks, 4);
+
   const handleStatusChange = (newStatus: string) => {
     setStatus(newStatus);
   };
@@ -24,7 +29,15 @@ const TodoApp = () => {
     <section className="mt-4 w-1/2 min-w-72 mx-auto flex flex-col justify-center items-center gap-3">
       <h2 className="text-slate-800 text-3xl font-bold">TODO LIST</h2>
       <TodoOptions status={status} onStatusChange={handleStatusChange} />
-      <TodoList tasks={filteredTasks} />
+      <TodoList tasks={currentPageData} />
+      {totalPages > 1 && (
+        <Pagination
+          className="todo__pagination"
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      )}
     </section>
   );
 };
