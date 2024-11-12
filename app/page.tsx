4e5e036a17c87686a587ba53966d262ff7875761
nima.yaghouti <nima.yaghouti99@gmail.com>
2024-11-12 09:06:07 +0330

    fix: change project title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ProgrammerIcon from "@/assets/svg/ProgrammerIcon";
 const Home = () => {
   return (
     <div className="pt-16 flex flex-col justify-center items-center text-center gap-8">
-      <h1 className="font-bold text-3xl">Welcome to NextTS Challenges</h1>
+      <h1 className="font-bold text-3xl">Welcome to NextJS+TS Challenges</h1>
       <div className="w-60 h-60 [&_*]:h-full [&_*]:w-full">
         <ProgrammerIcon />
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className="fixed top-0 left-0 right-0 bg-slate-900 text-white h-20 flex items-center justify-between px-8">
       <div className="flex items-center gap-2">
         <Image src={logo} alt="logo" className="w-6 h-6" />
-        <h2 className="text-2xl font-bold">NextTS Challenges</h2>
+        <h2 className="text-2xl font-bold">NextJS+TS Challenges</h2>
       </div>
       <nav className="space-x-4">
         <Link
